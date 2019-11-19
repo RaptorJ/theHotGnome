7924f0917fd5e6d509bd15aba804010e12a9d6e9
RaptorJ <fabcaron7@gmail.com>
2019-11-19 14:57:23 +0100

    Add models/article

---
+++ Project_G/models/article.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+var articleSchema = new Schema({
+  // _seller: { type: Schema.Types.ObjectId, ref: 'User' },
+  seller: { type: String, require: true },
+  title: { type: String, require: true },
+  content: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  price: { type: Number, require: true },
+  comments: [{
+    _writer: { type: Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  }]
+})
+
+const Article = mongoose.model('Article', articleSchema)
+module.exports = Article
