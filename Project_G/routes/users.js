3e5a112058a9acf0b7262bdf2dea1b3d7c0529e0
Thibault Frerejean <thibault.frerejean@gmail.com>
2019-11-19 15:54:12 +0100

    Add Users Route

--- Project_G/routes/users.js
+++ Project_G/routes/users.js
@@ -1,9 +1,19 @@
 const express = require('express')
-
+const bcrypt = require('bcrypt')
 var router = express.Router()
 
-router.use(express.static('views'))
+const User = require('../models/user.model.js')
 
+router.use(express.static('views'))
+/*
+async function tokenToUserMiddleware (req, res, next) {
+  if (req.session && req.session.userId) {
+    req.user = await User.findById(req.session.userId)
+  }
+  next()
+}
+*/
+/** ** route for log ** **/
 router.get('/login', (req, res) => {
   console.log('Get login page')
   res.render('login')
@@ -12,12 +22,67 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
   console.log(req.body)
   const { username, password } = req.body
+  /*
+  const user = await User.findOne({ username: username })
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.render(404).send('You are not registered ! Your username or password might be wrong !')
+    return
+  }
+  */
   if (username === '') {
     res.render('404')
   } else {
     console.log(`Post login page ${username}`)
     req.session.username = username
     res.render('index')
+  }
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.send('Disconnected!')
+})
+
+/** ** route for register ** **/
+router.get('/register', (req, res) => {
+  console.log('Get register page')
+  res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  console.log(req.body)
+  const { firstname, lastname, username, mail, password, birthday, street, city, postalCode, country } = req.body
+  if (!(username) || !(mail) || !(password)) {
+    res.status(403).send('You did not put enough information!')
+  }
+  const hash = await bcrypt.hash(password, 10)
+  /*
+  const user = await User.findOne({ username: username, mail: mail })
+  if (user) {
+    res.status(403).send('This username is already taken !')
+    return
+  }
+  */
+  try {
+    const newUser = new User({
+      firstname: firstname,
+      lastname: lastname,
+      username: username,
+      mail: mail,
+      password: hash,
+      birthday: birthday,
+      street: street,
+      city: city,
+      postalCode: postalCode,
+      country: country
+    })
+    // await newUser.save()
+    res.render('index')
+    console.log(`Post register page ${username}`)
+    req.session.username = newUser.username
+    return
+  } catch (err) {
+    res.status(404)
   }
 })
 
