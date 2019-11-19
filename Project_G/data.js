76bd735a4906baf6b3452a1d975c94ff3ced867d
Eliott Zimmermann <eliottzim@gmail.com>
2019-11-19 17:35:30 +0100

    Update data.js

--- Project_G/data.js
+++ Project_G/data.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 const User = require('./models/user.model.js')
 const Article = require('./models/article.model.js')
+const Role = require('./models/role.model.js')
+const Cart = require('./models/cart.model.js')
 mongoose.connect('mongodb://localhost/theHotGnome')
 
 fs.readFile('./data.json', 'utf8', (err, jsonString) => {
@@ -11,23 +13,46 @@ fs.readFile('./data.json', 'utf8', (err, jsonString) => {
   }
   try {
     const data = JSON.parse(jsonString)
-    console.log('Data to push in database: ', data)
+    // console.log('Data to push in database: ', data)
+    pushRoles(data)
     pushUsers(data)
     pushArticles(data)
-    console.log('All done!')
-    process.exit()
+    pushCarts(data)
   } catch (err) {
     console.log('Error parsing JSON string', err)
   }
 })
 
+function pushRoles (data) {
+  data.roles.forEach(async (role) => {
+    try {
+      const newRole = new Role({
+        name: role.name
+      })
+      await newRole.save()
+      console.log('Role ' + role.name + ' registered')
+    } catch (err) {
+      console.log('Error while registering user: ', err)
+    }
+  })
+}
+
 function pushUsers (data) {
   data.users.forEach(async (user) => {
     try {
+      let userRole
+      if (user._role === 'admin') userRole = await Role.findOne({ name: 'admin' })
+      else if (user._role === 'premium') userRole = await Role.findOne({ name: 'premium' })
+      else if (user._role === 'user') userRole = await Role.findOne({ name: 'user' })
+      else userRole = {}
       // const hashedPassword = await bcrypt.hash(user.password.toString(), 8)
       const newUser = new User({
         username: user.username,
-        password: user.password
+        password: user.password,
+        mail: user.mail,
+        birthDate: user.birthDate,
+        _role: userRole,
+        address: user.address
       })
       await newUser.save()
       console.log('User ' + user.username + ' registered')
@@ -50,6 +75,26 @@ function pushArticles (data) {
       })
       await newArticle.save()
       console.log('Article ' + article.title + ' registered')
+    } catch (err) {
+      console.log('Error while registering user: ', err)
+    }
+  })
+}
+
+function pushCarts (data) {
+  data.carts.forEach(async (cart) => {
+    try {
+      const cartUser = User.findOne({ username: cart._user })
+      cart.articles.forEach((article) => {
+        const cartArticles = []
+        cartArticles.push(Article.findOne({ title: article.title }))
+      })
+      const newCart = new Cart({
+        _user: cartUser,
+        articles: cartArticles
+      })
+      await newCart.save()
+      console.log('Cart with ' + cart.articles.stringify() + ' registered')
     } catch (err) {
       console.log('Error while registering user: ', err)
     }
