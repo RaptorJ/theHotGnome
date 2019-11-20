53fd8fb2adad79dd2abc87803bc14810137e93a8
RaptorJ <fabcaron7@gmail.com>
2019-11-20 12:20:30 +0100

    adding the different fonction for article managment

--- Project_G/routes/articles.js
+++ Project_G/routes/articles.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var router = express.Router()
 
-const Article = require('../models/article.model.js')
+const Article = require('../models/article.model')
 
 router.use(express.static('views'))
 
@@ -11,10 +11,42 @@ router.get('/new', (req, res) => {
   res.render('newArticle')
 })
 
+// route to get to the article informations
+router.get('/info/:title', async (req, res) => {
+  console.log('uptdate article page')
+  try {
+    const article = await Article.findOne({ title: req.param.title })
+    res.render('article', { article: article })
+    return
+  } catch (err) {
+    res.status(403).send(err)
+  }
+})
+
+// adding the item to the cart of the user connected
+router.post('/addToCart', async (req, res) => {
+  const { title, seller } = req.body
+  try {
+    const article = await Article.findOne({ title: title, seller: seller })
+    req.session.cart.push(article)
+    return
+  } catch (err) {
+    console.log(err)
+    res.status(403).send(err)
+  }
+})
+
+/** ** creating an article ** **/
 router.post('/new', async (req, res) => {
   const { seller, title, content, price, number } = req.body
   if (!(title) || !(seller) || !(content) || !(price) || !(number)) {
     res.status(403).send('You did not put enough information!')
+    return
+  }
+  const article = await Article.findOne({ title: title })
+  if (article) {
+    res.status(403).send('This item already exist.')
+    return
   }
   try {
     const newArticle = new Article({
@@ -25,14 +57,25 @@ router.post('/new', async (req, res) => {
       number: number
     })
     await newArticle.save()
-    console.log(newArticle)
     res.render('index')
-    // res.send('User registered!')
     console.log(`New article successfully added: ${title}`)
     return
   } catch (err) {
     res.status(404).render('404')
   }
+})
+
+// Uptade some informations (content or price) using the title (primary key)
+router.post('/update', async (req, res) => {
+  const { title, content, price } = req.body
+  try {
+    await Article.findOneAndUpdate({ title: title }, { content: content, price: price })
+  } catch (err) {
+    res.status(404).send(err)
+    return
+  }
+  const article = await Article.findOne({ title: title })
+  res.render('article', { article: article })
 })
 
 module.exports = {
