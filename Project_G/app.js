bfdabf38b1971a8eb4c2b95cc0c8d6d092f5d7fe
Alexis Bugielski <alexis.bugielski@gmail.com>
2019-11-21 19:55:05 +0100

    Add search bar with temporary products names

--- Project_G/app.js
+++ Project_G/app.js
@@ -19,7 +19,8 @@ app.use(bodyParser.json()) // for API requests
 
 app.use(session({
   name: 'sessId',
-  username: undefined
+  username: undefined,
+  secret: 'myDirtyLittleSecret'
 }))
 
 app.use('/users', users.router)
@@ -31,6 +32,41 @@ app.get('/', (req, res) => {
   console.log('OK')
   res.render('index')
   // res.send('ok')
+})
+
+app.get('/500', (req, res) => {
+  console.log('En développement')
+  res.render('500')
+  // res.send('ok')
+})
+
+app.get('/products', (req, res) => {
+  const availableTags = [
+    'Playstation 4 Pro',
+    'Switch',
+    'Asus Rog',
+    'Death Stranding',
+    'Dark Souls : Par delà la mort',
+    'Cyberpunk 2077',
+    'Tokyo Ghoul',
+    'Goblin Slayer',
+    'Iron Man',
+    'Monster Hunter : Iceborn',
+    'The Legend of Zelda : Breath of the Wild',
+    'The Legend of Zelda : Link\'s Awakening',
+    'Super Smash Bros Ultimate',
+    'Ace Combat 7 : Skies Unknown',
+    'Dark Souls III Design Works',
+    'Dark Souls I & II Design Works',
+    'Bloodborne Artbook officiel',
+    'Dark Souls : de Demon\'s Souls à Sekiro',
+    'Zelda : Hyrule Historia',
+    'Zelda : Art & Artifacs',
+    'Zelda : Encyclopedia',
+    'NieR : Automata World Guide',
+    'Lady Mechanika'
+  ]
+  res.send(availableTags)
 })
 
 app.listen(3000, () => {
